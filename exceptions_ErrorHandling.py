c59fb08b96cf3d841aab45ed3292984d6a25f283
kshitijbhure <34847295+kshitijbhure@users.noreply.github.com>
2017-12-26 00:08:04 -0600

    Update exceptions_ErrorHandling.py

--- exceptions_ErrorHandling.py
+++ exceptions_ErrorHandling.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python3
-# exceptions.py by Bill Weinman [http://bw.org/]
-# This is an exercise file from Python 3 Essential Training on lynda.com
-# Copyright 2010 The BearHeart Gorup, LLC
 
 
 try:
@@ -13,7 +9,7 @@ except IOError as e:
     print('Some error occurred. The error is {}'.format(e))
 
 except:
-    print('Some erorr occurred which is not an IO error.')
+    print('Some error occurred which is not an IO error.')
 
 
 print('after the error')
